c7105fd457f4dad1671e30f1a7425bf72dc8493f
knowankit <44323532+knowankit@users.noreply.github.com>
2019-12-23 12:46:17 +0530

    fix code

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react'
 import './App.scss'
 import 'antd/dist/antd.css'
 import {
-  BrowserRouter as Router, Switch, Route, Link
+  BrowserRouter as Router, Switch, Route
 } from 'react-router-dom'
 import Stepper from './components/stepper/stepper'
-import Home from './components/home'
+// import Home from './components/home'
 import Navbar from './components/navbar'
 
 function App () {
