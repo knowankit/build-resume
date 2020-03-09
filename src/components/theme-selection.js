499118b6d2bb0e9aad055c9a46ff621d2f9b3ff0
knowankit <44323532+knowankit@users.noreply.github.com>
2020-03-09 18:55:00 +0530

    change select theme component

--- src/components/theme-selection.js
+++ src/components/theme-selection.js
@@ -19,19 +19,56 @@ class ThemeSelection extends Component {
     if (this.state.redirect) {
     return <Redirect push to="/build" />;
     }
+
     return (
     <>
-      <div className='container theme-selection mt-2'>
-        <button onClick ={() => this.selectTheme('garden')}className='theme-button'>
+      <div className='theme-selection mt-2'>
+         <div className='heading'>
+          <p className='mx-auto'>Select theme</p>
+          </div>
+        <div className='grid-area'>
+          <button onClick ={() => this.selectTheme('garden')} className='theme-button'>
           Green
         </button>
+        <button onClick ={() => this.selectTheme('garden')} className='theme-button'>
+          Green
+        </button>
+        <button onClick ={() => this.selectTheme('garden')} className='theme-button'>
+          Green
+        </button>
+        <button onClick ={() => this.selectTheme('garden')} className='theme-button'>
+          Green
+        </button>
+        <button onClick ={() => this.selectTheme('garden')} className='theme-button'>
+          Green
+        </button>
+        <button onClick ={() => this.selectTheme('garden')} className='theme-button'>
+          Green
+        </button>
+        </div>
       </div>
       <style jsx>
         {`
           .theme-selection {
             background-color: white;
             border-radius: 5px;
+            width: 80%;
+            margin-left: 10%;
         }
+
+          .grid-area {
+            display: grid;
+            grid-template-columns: repeat(auto-fit, 180px);
+            grid-gap: 20px;
+            justify-content: center;
+            grid-template-rows: 200px;
+            margin-top: 20px;
+            grid-auto-rows: 200px;
+          }
+
+         .heading p{
+           color: black !important;
+         }
       `}
       </style>
     </>
