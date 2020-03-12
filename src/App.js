8a4c81d3125043b6b7ff0011eaa43b479aaf30a9
knowankit <44323532+knowankit@users.noreply.github.com>
2020-03-12 17:47:08 +0530

    add eslinit

--- src/App.js
+++ src/App.js
@@ -1,49 +1,48 @@
-import React from "react";
-import "./App.scss";
-import "antd/dist/antd.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./components/home";
-import Navbar from "./components/navbar";
-import Personal from "./containers/personal";
-import Profession from "./containers/profession";
-import Education from "./containers/education";
-import Projects from "./containers/projects";
-import Garden from "./containers/garden";
-import ThemeSelection from "./containers/theme-selection";
+import React from 'react';
+import './App.scss';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/home';
+import Navbar from './components/navbar';
+import Personal from './containers/personal';
+import Profession from './containers/profession';
+import Education from './containers/education';
+import Projects from './containers/projects';
+import Garden from './containers/garden';
+import ThemeSelection from './containers/theme-selection';
 
-function App() {
+function App () {
   return (
     <>
       <Navbar />
       <Router>
         <div>
           <Switch>
-            <Route exact path="/">
+            <Route exact path='/'>
               <Home />
             </Route>
-            <Route path="/theme-selection">
+            <Route path='/theme-selection'>
               <ThemeSelection />
             </Route>
-            <Route path="/personal">
+            <Route path='/personal'>
               <Personal />
             </Route>
-            <Route path="/profession">
+            <Route path='/profession'>
               <Profession />
             </Route>
-            <Route path="/education">
+            <Route path='/education'>
               <Education />
             </Route>
-            <Route path="/projects">
+            <Route path='/projects'>
               <Projects />
             </Route>
-            <Route path="/garden">
+            <Route path='/garden'>
               <Garden />
             </Route>
           </Switch>
         </div>
       </Router>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/home.js
+++ src/components/home.js
@@ -1,32 +1,29 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { Icon } from "antd";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
-  render() {
+  render () {
     return (
-      <div className="container">
-        <div className="heading">
-          <p className="mx-auto">Build your Resume in just few Clicks.</p>
-          <p className="mx-auto">
+      <div className='container'>
+        <div className='heading'>
+          <p className='mx-auto'>Build your Resume in just few Clicks.</p>
+          <p className='mx-auto'>
             Creating a Professional Resume and Cover Letter is Easy Now.
           </p>
-          <Link to="/theme-selection" className="btn btn-lg build-now">
+          <Link to='/theme-selection' className='btn btn-lg build-now'>
             Build Now
           </Link>
         </div>
-        <div className="row instruction-row">
-          <div className="instruction content mx-auto text-center mt-4">
+        <div className='row instruction-row'>
+          <div className='instruction content mx-auto text-center mt-4'>
             <p>
-              {" "}
-              <Icon type="appstore" theme="twoTone" /> &nbsp; Choose a Theme
+             Choose a Theme
             </p>
             <p>
-              {" "}
-              <Icon type="profile" theme="twoTone" /> &nbsp; Enter Details
+             Enter Details
             </p>
             <p>
-              <Icon type="file-pdf" theme="twoTone" /> &nbsp; Download PDF
+              Download PDF
             </p>
           </div>
         </div>
@@ -49,8 +46,8 @@ class Home extends Component {
           `}
         </style>
       </div>
-    );
+    )
   }
 }
 
-export default Home;
+export default Home
